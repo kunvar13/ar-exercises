a66cebf2023c811974b624c19bc3ad2604c17513
kunvar13 <kunvar13@gmail.com>
2022-01-04 19:44:46 -0600

    completed ex-2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 2: Update the first store
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Regina";
+
+@store1.save
